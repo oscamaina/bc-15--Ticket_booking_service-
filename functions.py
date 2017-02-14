f975f62671e04d0df6f413c6a8658c3f30bac931
oscamaina <mainaoscar40@gmail.com>
2017-02-14 17:34:04 +0300

    Adds program functions

--- functions.py
+++ functions.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import *
 
 def create_event(name, start, end, venue):
-	event_name = arg['<name>']
-	start_date = datetime.strptime (arg['<start_date>'], "%y/%m/%d") 
-	end_date = datetime.strptime (arg['<end_date>'], "%y/%m/%d")
-	venue = arg['<venue>']
+	event_name = ['<name>']
+	start_date = datetime.datetime.strptime (['<start_date>'], "%Y/%m/%d") 
+	end_date = datetime.datetime.strptime (['<end_date>'], "%Y/%m/%d")
+	venue = ['<venue>']
 	
 	new_event = Events(name=event_name,start_date=start_date, end_date=end_date,venue=venue)
 
@@ -16,11 +16,3 @@ def create_event(name, start, end, venue):
 	session.commit()
 	return "Created event"
 
-def delete_event(self):
-	deleted = session.query(Events).filter_by(id="event_id")
-	session.delete(deleted)
-	session.commit()
-	return "Deleted event"
-
-def list(self):
-	evts = session.query(Events).all()
